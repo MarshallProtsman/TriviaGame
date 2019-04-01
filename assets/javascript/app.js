ea69de57e5c8fa1fe9c0626b59c75fdb525baf69
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-03-31 23:46:07 -0400

    submission1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,38 @@
-//TIMER
 
+// making constants
 const quizContainer = document.getElementById("quiz");
 const resultsContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 const gameOver = document.getElementById("game-over")
 
+//create a timer function
 function startTimer(duration, display) {
+    //the timer is equal to the duration
   var timer = duration,
     minutes,
     seconds;
+    //
   setInterval(function() {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
-
+// If the minutes or seconds value is one digit, add a "0" before
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    display.textContent = minutes + ":" + seconds;
+    //display it
+    `<div class='time2'>
+    ${display.textContent = minutes + ":" + seconds}
+    </div>`
 
+    // what to do if the timer hits zero
     if (--timer < 0) {
       timer = duration;
+      //stop displaying the quiz body
       quizContainer.style.display = 'none';
       console.log("Quiz over");
+      //stop displaying the submit button
       submitButton.style.display = 'none;'
+      // show the game over text
       gameOver.style.display = 'inline';
       showResults();
     }
@@ -33,12 +43,13 @@ function startIt(x) {
     display = document.querySelector("#timer");
   startTimer(Minute * x, display);
   console.log(parseInt((Minute * x) / 60) + ":00");
+//   startTimer.style.display = "none";
 }
 
 //Calls the timer and passes the ammount of time
-startIt(1);
+startIt(.1);
 
-// QUESTIONS
+// ALL MY QUESTIONS AND ANSWERS IN AN ARRAY
 const myQuestions = [
   {
     question: "How many ounces are in a cappucino?",
@@ -69,22 +80,28 @@ const myQuestions = [
     correctAnswer: "c"
   },
   {
-      question: "Who is dwayne Shivers?",
+      question: "How old is Stanley?",
         answers: {
-            a: 'Micah Dalton',
-            b: 'Jeffery Scott',
-            c: 'Tim Apple'
+            a: '31',
+            b: '46',
+            c: '18'
         },
-        correctAnswer: 'a'
+        correctAnswer: 'c'
   }
 ];
 
+// a function to make a quiz. Called back to at the end
 function buildQuiz() {
 
+    //creating an array
   const output = [];
+
+  // loop that for each object in the array of q/a stuff, assigns it as current question and question number
   myQuestions.forEach((currentQuestion, questionNumber) => {
+      // create an array to contain answers
     const answers = [];
     for (letter in currentQuestion.answers) {
+        //pushes the 
       answers.push(
         `<label>
             <input type="radio" name="question${questionNumber}"
@@ -127,4 +144,8 @@ function showResults() {
 }
 
 buildQuiz();
-submitButton.addEventListener("click", showResults);
+submitButton.addEventListener("click", function(){
+showResults()
+submitButton.style.display = "none";
+});
+
